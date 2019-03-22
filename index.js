da60c9c19bc7513408d6c4a676de397f52bf39c2
Maxi Learner <sagmeister@gmail.com>
2019-03-22 15:14:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ function keepGoing(array, changeValue, skipValue) {
   return array
 }
 
-findBy(array, findFn) {
+findBy(array, findFn)
+
   for (let i = 0; i < array.length; i++) {
     if (array[i] !== findFn) {
       return null
