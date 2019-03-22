f57644e8a0691639bc032a34dab020357f978e82
Maxi Learner <sagmeister@gmail.com>
2019-03-22 14:58:41 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let i = 0; i < array.length; i++) {
+    array.forEach(array[i] => changeValue)
+    if (array[i] === stopValue) {
+      break
+    }
+  }
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0; i < array.length; i++) {
+    array.forEach(array[i] => changeValue)
+    if (array)
+}
+
+findBy(array, findFn) {
+  
+}
