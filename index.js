44b8698a34366290499ccab1319bf94206ebcf12
Maxi Learner <sagmeister@gmail.com>
2019-03-22 15:01:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
-    array.forEach(array[i] => changeValue)
+    array[i] = changeValue
     if (array[i] === stopValue) {
       break
     }
   }
 }
 
-function keepGoing(array, changeValue, skipValue) {
-  for (let i = 0; i < array.length; i++) {
-    array.forEach(array[i] => changeValue)
-    if (array)
-}
-
-findBy(array, findFn) {
-
-}
+// function keepGoing(array, changeValue, skipValue) {
+//   for (let i = 0; i < array.length; i++) {
+//     array.forEach(array[i] => changeValue)
+//     if (array)
+// }
+//
+// findBy(array, findFn) {
+//
+// }
